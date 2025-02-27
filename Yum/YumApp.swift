277924017a121b0f99cb917c78517f7a99f452fc
dljardim <dljardim@outlook.com>
2025-02-27 16:46:41 -0600

    Initial Commit

---
+++ Yum/YumApp.swift
@@ -0,0 +1,17 @@
+//
+//  YumApp.swift
+//  Yum
+//
+//  Created by Damian Jardim on 2/27/25.
+//
+
+import SwiftUI
+
+@main
+struct YumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
