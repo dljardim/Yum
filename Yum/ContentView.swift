56418fde487583ade8048ea1b37ec510b904e9f9
dljardim <dljardim@outlook.com>
2025-02-28 16:39:39 -0600

    - added assets - shows all menuItems using 2 ForEach views

--- Yum/ContentView.swift
+++ Yum/ContentView.swift
@@ -8,16 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+        
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List{
+                ForEach(menu){ section in
+                    Text(section.name)
+                    
+                    ForEach(section.items){ item in
+                        Text(item.name)
+                    }
+                }
+            }
         }
-        .padding()
     }
+    
 }
+
 
 #Preview {
     ContentView()
---
+++ Yum/_DevNotes.swift
@@ -0,0 +1,19 @@
+//
+//  _DevNotes.swift
+//  Yum
+//
+//  Created by Damian Jardim on 2/27/25.
+//
+
+import SwiftUI
+
+struct _DevNotes: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    _DevNotes()
+}
+
